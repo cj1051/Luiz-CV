fd7c701af1775314856dc13bf4c27ce3880bcac2
cj1051 <118214673+cj1051@users.noreply.github.com>
2023-01-12 20:12:12 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -20,19 +20,17 @@ setInterval(function () {
 
 
 //Animations
-gsap.registerPlugin(ScrollTrigger);
-
 gsap.fromTo("#hero", {autoAlpha: 0}, {autoAlpha: 1, duration: 2});
 
 gsap.fromTo("#hero", 
 {autoAlpha: 1} ,{
   scrollTrigger: {
     trigger: "#hero",
-    start: "bottom 95%",
+    start: "bottom 90%",
     end: "bottom center",
     pin: true,
     toggleActions: "restart pause reverse pause",
-    markers: true,
+    // markers: true,
     scrub: true
   },
   autoAlpha: 0,
@@ -55,19 +53,48 @@ containers.forEach((container) => {
   });
 });
 
-containers.forEach((container) => {
-  gsap.to(container, {
-    autoAlpha: 0,
-    ease: "power1.in",
-    duration: 2,
-    scrollTrigger: {
-      trigger: container,
-      start: "bottom 95%",
-      end: "bottom 20%",
-      //pin: true,
-      scrub: true,
-      toggleActions: "restart pause reverse pause",
+// containers.forEach((container) => {
+//   gsap.to(container, {
+//     autoAlpha: 0,
+//     ease: "power1.in",
+//     duration: 2,
+//     scrollTrigger: {
+//       trigger: container,
+//       start: "bottom 95%",
+//       end: "bottom 20%",
+//       //pin: true,
+//       scrub: true,
+//       toggleActions: "restart pause reverse pause",
+//     }
+//   });
+// });
+
+console.clear();
+
+gsap.utils.toArray(".card").forEach(function(card) {
+  gsap.set(card, {
+    transformStyle: "preserve-3d",
+    transformPerspective: 1000
+  });
+  const q = gsap.utils.selector(card);
+  const front = q(".cardFront");
+  const back = q(".cardBack");
+  
+  gsap.set(back, { rotationY:-180 });
+  
+  const tl = gsap.timeline({ paused: true })
+    .to(front, { duration: 1, rotationY: 180 })
+    .to(back, { duration: 1, rotationY: 0 }, 0)
+    .to(card, { z: 50 }, 0)
+    .to(card, { z: 0 }, 0.5);
+  card.addEventListener("click", () => {
+    if (tl.reversed()) {
+      tl.play();
+    } else {
+      tl.reverse();
     }
   });
+  // card.addEventListener("mouseleave", function() {
+  //   tl.reverse();
+  // });
 });
-
